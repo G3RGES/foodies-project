ae9a35077b7d04727bfa2680bc21af0dda870b8e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-11-30 12:54:15 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import MainHeader from "@/components/main-header";
 import "./globals.css";
-import MainHeaderBackground from "@/components/main-header-background";
+// import MainHeaderBackground from "@/components/main-header-background";
 
 export const metadata = {
   title: "NextLevel Food",
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <MainHeaderBackground />
+        {/* <MainHeaderBackground /> */}
 
         <MainHeader />
         {children}
