ba3234846f73a0911ca859bce66b4bebbe2e9891
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-11-30 13:04:10 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import MainHeader from "@/components/main-header";
+import MainHeader from "@/components/main-header/main-header";
 import "./globals.css";
 // import MainHeaderBackground from "@/components/main-header-background";
 
