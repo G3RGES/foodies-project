1a0bc21a682b303eb73a5b2f3f83b25128855a59
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-11-30 12:54:38 +0200

    Moving main-header files to a folder

---
+++ components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logo} alt="plate with food" priority />
+          Next Level Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
