a0ebf1ff7f7dbc3f86f05cbb259571c3704782c9
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-01 12:26:59 +0200

    Update page.js

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,25 @@
- export default function MealsPage(){
-    return <main>
-        <h1>Meals Page</h1>
-    </main>
- }
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, made{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p className="">
+          Choose your favorite recipe, and cook it yourself. it's easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+}
