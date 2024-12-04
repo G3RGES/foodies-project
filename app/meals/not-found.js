659321bb9910fc6f5c88ec25e2e116043e0e6bdc
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-04 17:34:30 +0200

    Create not-found.js

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfortunately, The meal you requested couldn&apos;t be found</p>
+    </main>
+  );
+}
