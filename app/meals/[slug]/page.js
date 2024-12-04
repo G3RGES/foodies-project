be3f0ac5ad87e6b8e9e21ae49cc3df1afaaeddd7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-04 16:42:55 +0200

    Update page.js

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,10 @@
+import classes from "./page.module.css";
+
 export default function MealPage({ params }) {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className=""></header>
+      <main className=""></main>
     </>
   );
 }
