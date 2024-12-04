708e6bc05899bb0e11689bbd6a1bffb806e3ff8d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-04 16:38:37 +0200

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, The page you requested couldn&apos;t be found</p>
+    </main>
+  );
+}
