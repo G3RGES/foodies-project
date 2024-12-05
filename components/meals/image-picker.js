933cb0f754f3e24a25525e199b67f13335e05123
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-05 14:12:46 +0200

    Update image-picker.js

---
+++ components/meals/image-picker.js
@@ -0,0 +1,31 @@
+"use client";
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const image = useRef();
+
+  function handlePick() {
+    image.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={image}
+        />
+
+        <button className={classes.button} type="button" onClick={handlePick}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}
