cea0f51062e593fa6cfea0715fc8de055115e645
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-06 17:04:05 +0200

    Update error.js

--- app/meals/error.js
+++ app/meals/error.js
@@ -3,7 +3,7 @@ export default function Error({ error }) {
   return (
     <main className="error">
       <h1>Error Occurred</h1>
-      <p>failed to fetch meal data, please try again later</p>
+      <p>Failed to fetch meal data, please try again later</p>
     </main>
   );
 }
