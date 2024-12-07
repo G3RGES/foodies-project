05e43fe2a8f499a269e33ddabfdb11514098c927
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-07 16:23:19 +0200

    Update page.js

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,19 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    describtion: meal.summary,
+  };
+}
+
 export default function MealPage({ params }) {
   const meal = getMeal(params.slug);
 
